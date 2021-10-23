ca65df25c460148305ff02f85e3bafa458220a15
saurabh28march <sg28march@gmail.com>
2021-10-23 20:34:01 +0530

    Customer Login Added

---
+++ app/src/main/java/com/example/dukaanApplication/CustomerLoginActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dukaanApplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomerLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_login)
+    }
+}
